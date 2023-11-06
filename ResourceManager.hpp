e3944478269413542399899f9eb0610878184293
jolaszulim <148874564+jolaszulim@users.noreply.github.com>
2023-11-06 19:43:20 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,18 +4,6 @@
 
 class ResourceManager
 {
-  public:
-    ResourceManager() : resource() {}
 
-    ResourceManager(const ResourceManager&) = default;
-    ResourceManager& operator=(const ResourceManager&) = default;
-    ResourceManager(ResourceManager&&) = default;
-    ResourceManager& operator=(ResourceManager&&) = default;
-    ~ResourceManager() = default;
-
-    double get() { return resource.get();}
-
-private:
-    Resource resource;
 
 };
