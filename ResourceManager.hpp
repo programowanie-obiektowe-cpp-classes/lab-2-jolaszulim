7f3282b9429d8f60c54281e860ad919d6253f3d3
jolaszulim <148874564+jolaszulim@users.noreply.github.com>
2023-11-07 10:36:52 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,6 +4,28 @@
 
 class ResourceManager
 {
+public:
+    ResourceManager() { r = new Resource(); }
+    ResourceManager(const ResourceManager& x) { r = new Resource(*x.r);}
+    ResourceManager& operator=(const ResourceManager& x) {
+        delete r;
+        r = new Resource(*x.r);
+        return *this;
+    }
+    ResourceManager(ResourceManager&& x) { 
+        r = x.r;
+        x.r = nullptr;
+    }
+    ResourceManager& operator=(ResourceManager&& x3) noexcept {
+        delete r;
+        r = x.r;
+        x.r = nullptr;
+        return *this;
+    }
+   double get() { return r->get();}
+   ~ResourceManager() { delete r; }
 
+private:
+    Resource* r;
 
 };
